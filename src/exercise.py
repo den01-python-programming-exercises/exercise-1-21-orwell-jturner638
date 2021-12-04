a261f614d4fab1825859ac0d7ef0988070ed9b05
Justin Turner <mrfathed@protonmail.com>
2021-12-04 11:07:23 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    number = int(input("Give a number:"))
+
+    if number == 1984:
+        print("Orwell")
 
 if __name__ == '__main__':
     main()
